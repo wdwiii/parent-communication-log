8e91393fceab57a0be20e936ed6c3f95090cad55
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-02 14:04:41 -0400

    Impt useState, ContactForm component & css module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import ContactForm from './ContactForm';
 
 function App() {
+  const [logData, setLogData] = useState([]);
+
   return (
     <div>
-
+      <ContactForm items={logData} />
     </div>
   );
 }
 
 export default App;
+imp;
