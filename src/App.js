e31aeb8085803c5c2a511bb0f6dd66f6203ff368
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-04 13:41:38 -0400

    Set initial state and pass to LogHistory via props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import ContactForm from './components/ContactForm/ContactForm';
 import LogHistory from './components/LogHistory/LogHistory';
 
 function App() {
-  const [logData, setLogData] = useState([]);
+  const [testData, setTestData] = useState([
+    {
+      firstname: 'Bobby',
+      lastname: 'Johnson',
+      studentid: '0233459',
+      grade: '09',
+      personContacted: 'James Johnson',
+      method: 'Phone',
+      concern: 'Bobby did not attend class for the sixth time in eight days.',
+    },
+    {
+      firstname: 'Angela',
+      lastname: 'Davis',
+      studentid: '0456342',
+      grade: '12',
+      personContacted: 'Monica Wallace',
+      method: 'Email',
+      concern: 'Received from other students of Angela verbally harassing them',
+    },
+  ]);
 
   return (
     <div>
       <h1 style={{ 'font-size': '24px', 'text-align': 'center' }}>
         Parent Communication Log
       </h1>
-      <ContactForm items={logData} />
-      <LogHistory />
+      <ContactForm />
+      <LogHistory items={testData} />
     </div>
   );
 }
