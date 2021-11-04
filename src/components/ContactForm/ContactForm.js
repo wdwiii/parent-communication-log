c955fddc595e48577a9150712e9939117965fcbe
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-04 10:18:23 -0400

    Import Card and Button components
    - Wrap form elements inside div w/ style prop

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,6 @@
 import styles from './ContactForm.module.css';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 const ContactForm = (props) => {
   const logContactHandler = (event) => {
@@ -7,75 +9,81 @@ const ContactForm = (props) => {
   };
 
   return (
-    <form
-      action=""
-      className={styles['contact-form']}
-      onSubmit={logContactHandler}
-    >
-      <label>
-        First Name:
-        <input type="text" id="form__fname" placeholder="Willie" />
-      </label>
-      {/* ============================ */}
-      <label>
-        Last Name:
-        <input type="text" id="form__lname" placeholder="Whitfield" />
-      </label>
-      {/* ============================ */}
-      <label>
-        Student ID:
-        <input type="number" id="form__id" placeholder="1234567" />
-      </label>
-      {/* ============================ */}
-      <label>
-        Grade:
-        <select id="form__grade" name="grade">
-          <option value="09">09</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-      </label>
-      {/* ============================ */}
-      <label>
-        Date:
-        <input type="date" id="form__date" />
-      </label>
-      {/* ============================ */}
-      <label>
-        Time:
-        <input type="time" id="form__time" />
-      </label>
-      {/* ============================ */}
-      <label>
-        Person Contacted:
-        <input
-          type="text"
-          id="form__contact-with"
-          placeholder="Mr. Whitfield"
-        />
-      </label>
-      {/* ============================ */}
-      <label>
-        Method of Contact:
-        <select id="form__grade" name="method">
-          <option value="email">Email</option>
-          <option value="fax">Fax</option>
-          <option value="in-person">In person Conversation</option>
-          <option value="student-delivered">
-            Student Notification Delivered
-          </option>
-          <option value="phone">Phone</option>
-          <option value="virtual">Virtual Platform</option>
-        </select>
-      </label>
-      {/* ============================ */}
-      <label>
-        Reason for Call:
-        <input type="textarea" id="form__reason" placeholder="Optional" />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <Card>
+      <form
+        action=""
+        className={styles['contact-form']}
+        onSubmit={logContactHandler}
+      >
+        <div style={{ 'padding-top': '2.5rem' }}>
+          <label>
+            First Name:
+            <input type="text" id="form__fname" placeholder="Willie" />
+          </label>
+          {/* ============================ */}
+          <label>
+            Last Name:
+            <input type="text" id="form__lname" placeholder="Whitfield" />
+          </label>
+          {/* ============================ */}
+          <label>
+            Student ID:
+            <input type="number" id="form__id" placeholder="1234567" />
+          </label>
+          {/* ============================ */}
+          <label>
+            Grade:
+            <select id="form__grade" name="grade">
+              <option value="09">09</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
+            </select>
+          </label>
+          {/* ============================ */}
+          <label>
+            Date:
+            <input type="date" id="form__date" />
+          </label>
+          {/* ============================ */}
+          <label>
+            Time:
+            <input type="time" id="form__time" />
+          </label>
+          {/* ============================ */}
+          <label>
+            Person Contacted:
+            <input
+              type="text"
+              id="form__contact-with"
+              placeholder="Mr. Whitfield"
+            />
+          </label>
+          {/* ============================ */}
+          <label>
+            Method of Contact:
+            <select id="form__grade" name="method">
+              <option value="email">Email</option>
+              <option value="fax">Fax</option>
+              <option value="in-person">In person Conversation</option>
+              <option value="student-delivered">
+                Student Notification Delivered
+              </option>
+              <option value="phone">Phone</option>
+              <option value="virtual">Virtual Platform</option>
+            </select>
+          </label>
+          {/* ============================ */}
+          <label>
+            Reason for Call:
+            <input type="textarea" id="form__reason" placeholder="Optional" />
+          </label>
+        </div>
+        <Button type="submit" background="rgb(0, 0, 82)">
+          Submit
+        </Button>
+      </form>
+    </Card>
   );
 };
 
