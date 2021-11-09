a4a85e011994943c14d7dc3f61ffa2e13cec43d3
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-09 11:22:06 -0500

    Un-nest input form inputs from label

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -16,68 +16,51 @@ const ContactForm = (props) => {
         onSubmit={logContactHandler}
       >
         <div style={{ width: '100%' }}>
-          <label>
-            First Name:
-            <input type="text" id="form__fname" placeholder="Willie" />
-          </label>
+          <label htmlFor="fname">First Name:</label>
+          <input type="text" id="fname" placeholder="Willie" />
           {/* ============================ */}
-          <label>
-            Last Name:
-            <input type="text" id="form__lname" placeholder="Whitfield" />
-          </label>
+          <label htmlFor="lname">Last Name: </label>
+          <input type="text" id="lname" placeholder="Whitfield" />
           {/* ============================ */}
-          <label>
-            Student ID:
-            <input type="number" id="form__id" placeholder="1234567" />
-          </label>
+          <label htmlFor="id">Student ID:</label>
+          <input type="number" id="id" placeholder="1234567" />
           {/* ============================ */}
-          <label>
-            Grade:
-            <select id="form__grade" name="grade">
-              <option value="09">09</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-            </select>
-          </label>
+          <label htmlFor="grade"> Grade:</label>
+          <select id="grade" name="grade">
+            <option value="09">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
           {/* ============================ */}
-          <label>
-            Date:
-            <input type="date" id="form__date" />
-          </label>
+          <label htmlFor="date">Date:</label>
+          <input type="date" id="date" />
           {/* ============================ */}
-          <label>
-            Time:
-            <input type="time" id="form__time" />
-          </label>
+          <label htmlFor="time">Time:</label>
+          <input type="time" id="time" />
           {/* ============================ */}
-          <label>
-            Person Contacted:
-            <input
-              type="text"
-              id="form__contact-with"
-              placeholder="Mr. Whitfield"
-            />
-          </label>
+          <label htmlFor="contact"> Person Contacted: </label>
+          <input type="text" id="contact" placeholder="Mr. Whitfield" />
           {/* ============================ */}
-          <label>
-            Method of Contact:
-            <select id="form__grade" name="method">
-              <option value="email">Email</option>
-              <option value="fax">Fax</option>
-              <option value="in-person">In person Conversation</option>
-              <option value="student-delivered">
-                Student Notification Delivered
-              </option>
-              <option value="phone">Phone</option>
-              <option value="virtual">Virtual Platform</option>
-            </select>
-          </label>
+          <label id="method">Method of Contact:</label>
+          <select id="method" name="method">
+            <option value="email">Email</option>
+            <option value="fax">Fax</option>
+            <option value="in-person">In person Conversation</option>
+            <option value="student-delivered">
+              Student Notification Delivered
+            </option>
+            <option value="phone">Phone</option>
+            <option value="virtual">Virtual Platform</option>
+          </select>
           {/* ============================ */}
-          <label>
-            Reason for Call:
-            <input type="textarea" id="form__reason" placeholder="Optional" />
-          </label>
+          <label>Reason for Call:</label>
+          <input
+            type="textarea"
+            id="concern"
+            name="concern"
+            placeholder="Optional"
+          />
         </div>
         <Button type="submit" background="rgb(0, 0, 82)">
           Submit
