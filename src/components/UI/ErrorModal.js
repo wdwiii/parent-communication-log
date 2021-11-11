ca6ea004578e99e338df6a3718055d0339cb0068
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-11-11 15:38:52 -0500

    Add wrapper div and .backdrop to ErrorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,17 +4,20 @@ import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button>Close</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={classes.backdrop}></div>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button>Close</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
